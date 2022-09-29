53897f8a8185faa253d128471ca6e304e5667273
Alejandro Mígues <migues2000@Alejandros-MacBook-Pro.local>
2022-09-29 16:57:23 -0300

    Added Tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,8 @@ import type { NextPage } from 'next';
 
 const HomePage: NextPage = () => {
   return (
-    <div>
-      <h1>Link Shortener</h1>
+    <div className='flex items-center justify-center w-full h-full'>
+      <h1 className='text-4xl font-bold text-primary-500'>Link Shortener</h1>
     </div>
   );
 };
