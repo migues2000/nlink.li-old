c25f316a38186f1244ab1d18f40d11d03ad5dd85
Alejandro Mígues <migues2000@Alejandros-MacBook-Pro.local>
2022-10-02 16:29:59 -0300

    Link validation

--- src/components/LinkInput.tsx
+++ src/components/LinkInput.tsx
@@ -1,8 +1,32 @@
 import { LinkIcon } from '@heroicons/react/20/solid';
 import { default as useShortener } from '@hooks/useShortener';
+import { ChangeEventHandler, useState } from 'react';
 
 const LinkInput = () => {
   const { setLink } = useShortener();
+  const [isValid, setIsValid] = useState<boolean>();
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    event.target.value !== ''
+      ? isValidLink(event.target.value)
+        ? (setLink(event.target.value), setIsValid(true))
+        : (setLink(undefined), setIsValid(false))
+      : setIsValid(undefined);
+  };
+
+  const isValidLink = (link: string) => {
+    const urlRegex = new RegExp(
+      '^(https?:\\/\\/)?' + // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+#]*)*' + // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+        '(\\#[-a-z\\d_]*)?$',
+      'i'
+    ); // fragment
+    return urlRegex.test(link);
+  };
+
   return (
     <div className='relative w-full'>
       <div className='absolute top-0 bottom-0 w-5 h-5 my-auto left-3'>
@@ -10,13 +34,24 @@ const LinkInput = () => {
       </div>
       <input
         type='url'
-        onChange={(event) =>
-          setLink(event.target.value !== '' ? event.target.value : undefined)
+        onChange={handleChange}
+        className={
+          inputStyles[
+            isValid === undefined ? 'neutral' : isValid ? 'valid' : 'invalid'
+          ]
         }
-        className='w-full py-2 pl-10 pr-2 transition-colors border rounded-lg shadow-xl bg-base-50 border-base-200 shadow-base-100 focus:outline-none focus:border-primary-300'
       />
     </div>
   );
+};
+
+const inputCommonStyles =
+  'w-full py-2 pl-10 pr-2 transition-colors border rounded-lg shadow-xl bg-base-50 border-base-200 shadow-base-100 focus:outline-none';
+
+const inputStyles = {
+  neutral: `focus:border-primary-300 ${inputCommonStyles}`,
+  valid: `border-green-500 ${inputCommonStyles}`,
+  invalid: `border-red-500 ${inputCommonStyles}`,
 };
 
 export default LinkInput;
