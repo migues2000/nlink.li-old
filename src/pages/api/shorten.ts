d51bf3fe35fe858c02edd06a4ad99590d5990bee
Alejandro Mígues <migues2000@Alejandros-MacBook-Pro.local>
2022-10-09 10:15:39 -0300

    Automatic link deletion

--- src/pages/api/shorten.ts
+++ src/pages/api/shorten.ts
@@ -1,6 +1,7 @@
 import type { NextApiHandler } from 'next';
 import { default as prisma } from '@lib/prisma';
 import { nanoid } from 'nanoid';
+import { scheduleJob } from 'node-schedule';
 
 const handler: NextApiHandler = async (req, res) => {
   if (req.method !== 'POST') return res.status(404).end();
@@ -11,6 +12,21 @@ const handler: NextApiHandler = async (req, res) => {
     const shortenedLink = await prisma.shortenedLink.create({
       data: { ...input, id: nanoid(4) },
     });
+    scheduleJob(
+      new Date(
+        shortenedLink.shortened_at.getTime() +
+          Number(process.env.NEXT_PUBLIC_SHORTENED_LINK_EXPIRATION_TIME)
+      ),
+      async () => {
+        try {
+          await prisma.shortenedLink.delete({
+            where: { id: shortenedLink.id },
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    );
     return res.status(201).send(`${process.env.DOMAIN}/${shortenedLink.id}`);
   } catch (error) {
     console.log(error);
