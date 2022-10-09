a7be8047a685cb9b4390b37f76461048375c5c11
Alejandro Mígues <migues2000@Alejandros-MacBook-Pro.local>
2022-10-09 10:40:13 -0300

    From context API to Zustand

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,11 @@
 import type { NextPage } from 'next';
 import { default as PageLayout } from '@components/layout/PageLayout';
-import { ShortenerContextProvider } from '@contexts/ShortenerContext';
 import { default as Shortener } from '@components/Shortener';
-import { default as PasswordDialog } from '@components/PasswordDialog';
 
 const HomePage: NextPage = () => {
   return (
     <PageLayout title='Link Shortener'>
-      <ShortenerContextProvider>
-        <Shortener />
-      </ShortenerContextProvider>
+      <Shortener />
     </PageLayout>
   );
 };
