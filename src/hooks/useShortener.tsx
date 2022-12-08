00a95ed9aac723efa0b758c3dcf43af2589d7634
Alejandro Mígues <migues2000@192.168.1.6>
2022-12-07 22:09:27 -0300

    Revert "Minor fixes"
    This reverts commit 5b189c0fad5848afe165e0d969db4e415df63d0c.

--- src/hooks/useShortener.tsx
+++ src/hooks/useShortener.tsx
@@ -38,7 +38,7 @@ const useShortener = create<ShortenerState>()(
 
           if (result.status !== 201) throw new Error('Something went wrong');
 
-          set({ link: undefined, password: undefined, isSensitive: false });
+          set({ link: undefined });
           return await result.text();
         },
       }),
